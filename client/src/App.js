576d35b0eb020771ff1c0b7d82f4c796b7ac2558
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-08-11 11:48:38 -0400

    About page is functional

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
+import About from "./containers/About/About";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <NavBar />
       <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
     </Router>
   );
 }
