6c81bc8779093e712f062fbccfbfeb1a2e226d74
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-08-12 19:11:06 -0400

    updated icons

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
